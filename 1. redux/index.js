819cfa7154d0f6411bddd8691a91dff7d6708bab
JuicyJerry <Ju2cyjerry@gmail.com>
2021-04-17 20:05:15 +0900

    [redux] redux study - action creator and reducer

--- 1. redux/index.js
+++ 1. redux/index.js
@@ -1,14 +1,35 @@
 const { createStore } = require("redux");
 
-const reducer = () => {};
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "CHANGE_COMP_A":
+      return {
+        compA: action.data, // action name
+        compB: 12,
+        compC: null,
+      };
+  }
+};
 const initialState = {
   comA: "a",
   comB: 12,
   comC: null,
 };
 
-initialState.comA = "b"; // 이렇게 사용하면 안 된다
+// initialState.comA = "b"; // 이렇게 사용하면 안 된다
 
 const store = createStore(reducer, initialState);
 
-console.log(store.getState()); // state가 나와요
+console.log("1: " + store.getState()); // state가 나와요
+
+// action을 만드는 creator: 액션 생성자
+const changeCompA = (data) => {
+  return {
+    type: "CHANGE_COMP_A", // action name
+    data,
+  };
+};
+
+store.dispatch(changeCompA("b"));
+
+console.log("2: " + store.getState());
