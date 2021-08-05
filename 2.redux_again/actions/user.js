a31556e912698facb20879ae4dccf720e3e2c581
JuicyJerry <Ju2cyjerry@gmail.com>
2021-08-05 23:51:41 +0900

    apply middleware

--- 2.redux_again/actions/user.js
+++ 2.redux_again/actions/user.js
@@ -1,3 +1,29 @@
+const logIn = () => {
+  // async action creator
+  return (dispatch, getState) => {
+    dispatch(logInRequest());
+    setTimeout(() => {
+      // 서버가 없어서
+      dispatch(loginSuccess());
+    }, 2000);
+  };
+};
+
+/*
+ 어떻게 보면 비동기 처리도 눈속임이에요. 
+ 비동기 액션 크리에이터라는 기능을 미들웨어를 통해 추가하고
+ 동기 한 번 부르고 비동기작업안에서 끝날쯤에 동기 한번 더 부르고
+ 동기 액션들간에 실행 순서를 조작하는 정도밖에 안 되는거에요.
+*/
+
+const logInRequest = () => {
+  return {};
+};
+
+const loginSuccess = () => {
+  return {};
+};
+
 const logIn = (data) => {
   return {
     type: "LOG_IN",
--- 2.redux_again/index2.js
+++ 2.redux_again/index2.js
@@ -1,5 +1,5 @@
 //! 데이터 중심 사고를 해야한다! (화면은 안 중요해요. 리덕스의 장점이 데이터가 바뀌면 알아서 바뀌는거잖아요 )
-const { createStore } = require("redux");
+const { createStore, applyMiddleware, compose } = require("redux");
 const reducer = require("./reducers");
 const { addPost } = require("./actions/post");
 const { logIn, logOut } = require("./actions/user");
@@ -18,9 +18,38 @@ const initialState = {
   followers: [],
 };
 
-const store = createStore(reducer, {
-  initialState,
-});
+// 기능 증강 : enhancer
+// 함수 합성 함수 : compose (combineReducer랑 비슷?)
+
+const firstMiddleware = (store) => (dispatch) => (action) => {
+  console.log("로깅:", action);
+  // 기능 추가
+  dispatch(action);
+  // 기능 추가
+};
+/*
+  위 구문과 동일함
+  function firstMiddleware() {
+    return function (next) {
+      return function (action) {};
+    };
+  }
+*/
+
+const thunkMiddleware = (store) => (dispatch) => (action) => {
+  if (typeof action === "function") {
+    // 비동기를 구별해주기 위해, 비동기인 경우에는 액션을 함수로 만들어주겠다 라는 썽크에서의 약속
+    // 객체가 현재 동기적으로 동작하고 있고 객체로도 비동기처리를 할수 있긴하다고 한다.
+
+    return action(store.dispatch, store.getState); // 얘네들은 어디에 전달되냐?
+  }
+  return dispatch(action);
+};
+
+// const enhancer = compose(applyMiddleware());
+const enhancer = applyMiddleware(firstMiddleware, thunkMiddleware);
+
+const store = createStore(reducer, initialState, enhancer);
 
 // View(화면은 언제 바뀌어요): 아래 subscribe가 react-redux안에 들어있어서 따로 써줄필요없다.
 store.subscribe(() => {
